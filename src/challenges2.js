e1b10ef02e17a0c2cd5bac2759132afcfec4383b
DanielFerreira <daniel18mtm@gmail.com>
2023-05-16 12:02:13 -0300

    Requisito 12 bônus feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,6 +2,13 @@
 
 // Desafio 12 -  Crie a função triangleCheck
 
+let triangleCheck = function(lineA, lineB, lineC){
+if(lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB && lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineB - lineA)){
+  return true
+}else{return false};
+};
+
+
 // Desafio 13 - Crie a função hydrate
 
 /* eslint no-undef: 0 */
